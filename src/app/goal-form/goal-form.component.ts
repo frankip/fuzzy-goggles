b21e9ad56b8fe0e425452ba92bc5b9e91acada40
Francky <gwadafrank@gmail.com>
2020-11-04 23:34:23 +0300

    form component - ngModel - formvalidation - submit form

---
+++ src/app/goal-form/goal-form.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit, Output, EventEmitter} from '@angular/core';
+import { Goal } from '../goal';
+
+@Component({
+  selector: 'app-goal-form',
+  templateUrl: './goal-form.component.html',
+  styleUrls: ['./goal-form.component.css']
+})
+export class GoalFormComponent implements OnInit {
+
+  newGoal = new Goal(0,"","",new Date());
+  @Output() addGoal = new EventEmitter<Goal>();
+
+  submitGoal(){
+    console.log("locked and ready");
+    
+    this.addGoal.emit(this.newGoal);
+
+    this.newGoal = new Goal(0,"","",new Date());
+    
+  }
+
+  // console.log(newGoal);
+  
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/goal/goal.component.ts
+++ src/app/goal/goal.component.ts
@@ -10,12 +10,10 @@ import { Goal } from '../goal';
 export class GoalComponent implements OnInit {
 
   goals: Goal [] = [
-    new Goal(1, 'Watch finding Nemo', 'Find an online version and watch merlin find his son'),
-    new Goal(2,'Buy Cookies','I have to buy cookies for the parrot'),
-    new Goal(3,'Get new Phone Case','Diana has her birthday coming up soon'),
-    new Goal(4,'Get Dog Food','Pupper likes expensive snacks'),
-    new Goal(5,'Solve math homework','Damn Math'),
-    new Goal(6,'Plot my world domination plan','Cause I am an evil overlord'),
+    new Goal(3,'Get new Phone Case','Diana has her birthday coming up soon',new Date(2022,1,12)),
+    new Goal(4,'Get Dog Food','Pupper likes expensive snacks',new Date(2019,0,18)),
+    new Goal(5,'Solve math homework','Damn Math',new Date(2019,2,14)),
+    new Goal(6,'Plot my world domination plan','Cause I am an evil overlord',new Date(2030,3,14)),
   ]
 
   toggleDetails(index){
@@ -36,6 +34,18 @@ export class GoalComponent implements OnInit {
       if (toDelete){
         this.goals.splice(index, 1);
       }
+    }
+  }
+
+  addNewGoal(goal:Goal){
+    console.log(goal);
+
+    if(goal){
+
+      let goalLength = this.goals.length
+      goal.id = goalLength + 1;
+      // goal.completeDate = new Date(goal.completeDate)
+      this.goals.push(goal);
     }
   }
 
